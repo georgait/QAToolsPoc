98fcb10f6df2f3b0d55f2c5f04aad85d249999f4
georgait <giorgat88@gmail.com>
2023-03-26 23:25:06 +0300

    improve formatting

--- QATools/Pattern/IActor.cs
+++ QATools/Pattern/IActor.cs
@@ -4,7 +4,6 @@ public interface IActor
 {
     string Name { get; }
     IReadOnlyCollection<IAbility> Abilities { get; }
-
     IAbility HasTheAbilityTo(IAbility ability);
     Task<T> WhoAsksFor<T>(IQuestion<T> question) where T : class;
     Task WhoAttemptsTo(ITask task);

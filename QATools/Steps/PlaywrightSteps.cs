310f78ed64afd50f8ae1844ad7ee872c685dcd6a
georgait <giorgat88@gmail.com>
2023-03-29 00:42:30 +0300

    adjust new methods

--- QATools/Steps/PlaywrightSteps.cs
+++ QATools/Steps/PlaywrightSteps.cs
@@ -13,6 +13,8 @@ public class PlaywrightSteps
             .AsyncWith(Browsers.CHROMIUM, opt =>
             {
                 // options from appsettings.json (optional, default values are used if not provided)
+                opt.Headless = false;
+                opt.TraceViewEnabledOnFailure = false;
             });
         
         await actor.WhoAttemptsTo(Navigate.To(Urls.DOC));
@@ -21,30 +23,31 @@ public class PlaywrightSteps
     [When(@"(.*) searches for ""([^""]*)""")]
     public async Task WhenGeorgeSearchesFor(IActor actor, string text)
     {
-        await actor.WhoAttemptsTo(Click.OnTarget().UsingDynamicLocator(TopNavBar.SearchLabelBtn));
-        await actor.WhoAttemptsTo(Enter.TheValue(text).UsingDynamicLocator(TopNavBar.SearchDocs));
+        await actor.WhoAttemptsTo(Click.OnTarget(TopNavBar.SearchLabelBtn));
+
+        await actor.WhoAttemptsTo(Enter.OnTarget(TopNavBar.SearchDocs).TheValue(text));
     }
 
     [Then(@"(.*) clicks on GetStarted button in homepage")]
     public async Task ThenGeorgeClicksOnGetStartedButtonInHomepage(IActor actor)
     {
-        await actor.WhoAttemptsTo(Click.OnTarget().UsingDynamicLocator(HomePage.GetStartedBtn));
+        await actor.WhoAttemptsTo(Click.OnTarget(HomePage.GetStartedBtn));
     }
 
     [Then(@"(.*) navigates to trace viewer via sidebar")]
     public async Task ThenGeorgeNavigatesToTraceViewerViaSidebar(IActor actor)
     {
-        await actor.WhoAttemptsTo(Click.OnTarget().UsingDynamicLocator(SideNavBar.TraceViewerBtn));
+        await actor.WhoAttemptsTo(Click.OnTarget(SideNavBar.TraceViewerBtn));
     }
 
     [Then(@"(.*) asserts that the viewing trace cli command is ""([^""]*)""")]
     public async Task ThenGeorgeAssertsThatTheViewingTraceCliCommandIs(IActor actor, string command)
     {
         // 1. With Page object
-        //var text = await actor.WhoAsksFor(TheText.OfLocator().Using(TraceViewer.CliCommand));
-        
+        //var text = await actor.WhoAsksFor(TheBetterText.OfTarget(TraceViewer.CliCommand));
+
         // 2. With Func
-        var text = await actor.WhoAsksFor(TheText.OfTarget().UsingDynamicLocator(page =>
+        var text = await actor.WhoAsksFor(TheText.OfTarget(page =>
         {
             return page.GetByRole(AriaRole.Code).Filter(new() { HasText = command }).First;
         }));        
@@ -55,7 +58,7 @@ public class PlaywrightSteps
     [Then(@"(.*) navigates to trace viewer static website")]
     public async Task ThenGeorgeNavigatesToTraceViewerStaticWebsite(IActor actor)
     {
-        await actor.WhoAttemptsTo(OpenNewWindow.Named("TraceViewerStaticWebsite").UsingDynamicLocator(TraceView.Page));
+        await actor.WhoAttemptsTo(OpenNewWindow.Named("TraceViewerStaticWebsite").UsingTarget(TraceView.Page));
     }
 }
 
